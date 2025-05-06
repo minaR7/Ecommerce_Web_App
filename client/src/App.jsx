6b3fd8b598ac04ae3a503fae70680b139bb72794
momina7R <syedamominarashid752@gmail.com>
2025-05-06 09:56:44 +0500

    Added routes in App.jsx and refactored the app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { Layout, Menu, Drawer, Button, Card, Row, Col } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
-
+import HeaderMenu from './components/Header';
+import MainContent from './components/MainContent';
+import FooterMenu from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Men from './pages/Men';
+import Women from './pages/Women';
+import MenSubcategory from './pages/MenSubcategory';
+import WomenSubcategory from './pages/WomenSubcategory';
+import Accessories from './pages/Accessories';
+import Kids from './pages/Kids';
+import NotFound from './pages/404';
+import MyAccount from './pages/Login';
+import ProductDetail from './pages/ProductDetail';
 const { Header, Content, Footer } = Layout;
 
 const App = () => {
@@ -11,45 +24,28 @@ const App = () => {
   const closeDrawer = () => setVisible(false);
 
   return (
-    <Layout>
-      <Header style={{
-          position: 'sticky',
-          top: 0,
-          zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          height: '70px',
-        }}>
-        <Button type="text" icon={<MenuOutlined />} onClick={showDrawer} />
-        <Drawer placement="left" onClose={closeDrawer} visible={visible}>
-          <Menu mode="inline">
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Shop</Menu.Item>
-            <Menu.Item key="3">About Us</Menu.Item>
-            <Menu.Item key="4">Contact</Menu.Item>
-          </Menu>
-        </Drawer>
-      </Header>
+     <Layout style={{ backgroundColor: "#ffffff"}} >
+        <HeaderMenu> </HeaderMenu>
+        <Content style={{ padding: '0rem 0rem 1rem 0rem', minHeight: "75vh", backgroundColor: "#fff" }}> 
+          {/* {marginTop: 64, #f5f5f5 } */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/men" element={<Men />} />
+              <Route path="/men/:subcategory" element={<MenSubcategory />} />
+              <Route path="/women" element={<Women />} />
+              <Route path="/women/:subcategory" element={<WomenSubcategory />} />
+              <Route path="/kids" element={<Kids />} />
+              <Route path="/accessories" element={<Accessories />} />
+              <Route path="/my-account" element={<MyAccount />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
 
-      <Content style={{ padding: '0 50px', marginTop: 64, minHeight: "78vh" }}>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Jabador" bordered={false}>Product 1</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Gandoura" bordered={false}>Product 2</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Djellaba" bordered={false}>Product 3</Card>
-          </Col>
-        </Row>
-      </Content>
-
-      <Footer style={{ textAlign: 'center', height: '4rem', background: 'aquamarine', }}>
-        © 2025 Jabador. All rights reserved.
-      </Footer>
+              {/* Catch-all route */}
+              <Route path="*" element={<NotFound />} />
+          </Routes>
+         </Content>
+      <FooterMenu></FooterMenu>
     </Layout>
+
   );
 };
 
