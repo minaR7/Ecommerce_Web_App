2bd5bf7f46ee5b86bae32ce3ff6fc6e36551fdb3
momina7R <syedamominarashid752@gmail.com>
2025-05-17 18:41:13 +0500

    Refactored cart routes to update a cart item

--- server/routes/addToCartRoutes.js
+++ server/routes/addToCartRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/addToCartController');
 
-router.post('/', cartController.addToCart);
+router.post('/add', cartController.addToCart);
+router.post('/add/bulk', cartController.addBulkToCart);
 router.get('/:userId', cartController.getCart);
+router.put('/:cartItemId', cartController.updateCartItem);
 router.delete('/:cartItemId', cartController.removeFromCart);
 
 module.exports = router;
