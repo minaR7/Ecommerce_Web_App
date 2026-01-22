74ff306e1e654b1dec8ddc46bd67ca064e285d5f
momina7R <108214899+minaR7@users.noreply.github.com>
2026-01-22 20:43:46 +0500

    Refactored API routes
    - Orders: Added new routes to fetch order
    - Coupons: Updated routes to fetch, add and validate coupons
    - Shipping: Added new routes for shipping CRUD
    - Products: Refactored product get route

--- server/routes/coupon.js
+++ server/routes/coupon.js
@@ -2,5 +2,11 @@ const express = require('express');
 const couponController = require('../controllers/couponController');
 const router = express.Router();
 
+router.get('/', couponController.getCoupons);
+router.get('/:id', couponController.getCouponById);
+router.post('/', couponController.createCoupon);
+router.put('/:id', couponController.updateCoupon);
+router.delete('/:id', couponController.deleteCoupon);
+router.get('/validate/:code', couponController.validateCoupon);
 router.post('/validate-coupon', couponController.validateCoupon);
 module.exports = router;
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const router = express.Router();
 
-router.get('/', productController.getProducts);
+// router.get('/', productController.getProducts);
+router.get('/', productController.getAllProducts);
 router.get('/search', productController.searchProducts);
 router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
