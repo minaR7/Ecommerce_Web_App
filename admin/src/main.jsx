96f91234628d23012a2cd269e2017ea8a4789f88
momina7R <108214899+minaR7@users.noreply.github.com>
2026-01-22 21:24:05 +0500

    App base files added
    - Added browser routes
    - Configured theme and layout
    - Added pages
    - Integrated protected routes
    - UI updates

---
+++ admin/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux'; // Redux Provider
+import store from './redux/store';      // Your Redux store
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     {/*<Provider store={store}> Wrap app in Redux Provider */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </Provider> */}
+  </StrictMode>
+);
+
