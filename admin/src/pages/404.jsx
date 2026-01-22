e5a9977d9c5ccef8a1792b279b635a98cac63e10
momina7R <108214899+minaR7@users.noreply.github.com>
2026-01-22 21:45:46 +0500

    Create 404.jsx

---
+++ admin/src/pages/404.jsx
@@ -0,0 +1,14 @@
+// pages/404.jsx
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '40px' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <Link to="/">Go back to Home</Link>
+        </div>
+    );
+};
+
+export default NotFound;
