a08f5e5838b65e50abadfb5702e12fb3dabca352
momina7R <108214899+minaR7@users.noreply.github.com>
2026-01-24 00:26:06 +0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.registerUser);
 router.get('/', userController.getUsers);
+router.get('/customers', userController.getCustomers);
 router.post('/login', userController.loginUser);
 
 module.exports = router;
