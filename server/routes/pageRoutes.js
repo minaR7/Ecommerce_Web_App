8964ffca026b2789b73f0085dee107018f0a3794
MominaRashid <108214899+minaR7@users.noreply.github.com>
2026-02-10 11:17:49 +0500

    Create pageRoutes.js

---
+++ server/routes/pageRoutes.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const router = express.Router();
+const pageController = require('../controllers/pageController');
+
+router.get('/', pageController.getPages);
+router.get('/:slug', pageController.getPageBySlug);
+router.put('/:slug', pageController.updatePage);
+
+module.exports = router;
