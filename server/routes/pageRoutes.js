01ab77d446a1eb60ea9e3dae5d575f25b62d53e6
momina7R <108214899+minaR7@users.noreply.github.com>
2026-02-14 20:02:24 +0500

    Update pageRoutes.js

--- server/routes/pageRoutes.js
+++ server/routes/pageRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const pageController = require('../controllers/pageController');
 
 router.get('/', pageController.getPages);
+router.post('/', pageController.createPage);
 router.get('/:slug', pageController.getPageBySlug);
 router.put('/:slug', pageController.updatePage);
 
